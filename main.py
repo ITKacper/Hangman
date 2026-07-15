70e3ff153caaad19f296b6593216783c197f891f
itKacper <kacpi.s@onet.pl>
2026-07-15 22:38:17 +0200

    encryption/decryption

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 import math
 import random
 import time
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
+print(chars)
+print(key)
+
+encryptedword = []
+
+
+while True:
+    chose = input("1. Encrypt\n2. Decrypt\n3. Exit\n ")
+    if chose == "1":
+        word = input("Type word to encrypt: ")
+        for i in range(len(word)):
+            index = chars.index(word[i])
+            encryptedword.append(key[index])
+        for char in encryptedword:
+            print(char, end="")
+        print()
+        encryptedword.clear()
+
+    elif chose == "2":
+        word = input("Type word to decrypt: ")
+        for i in range(len(word)):
+            index = key.index(word[i])
+            encryptedword.append(chars[index])
+        for char in encryptedword:
+            print(char, end="")
+        print()
+        encryptedword.clear()
+    elif chose == "3":
+        break
+    else:
+        print("Invalid input")
 
